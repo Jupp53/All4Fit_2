637954ae358cd6c0bdfedebcc79ba84844d91a93
Jupp53 <58737962+Jupp53@users.noreply.github.com>
2022-01-26 20:18:00 +0100

    Add files via upload

---
+++ test.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using All4Fit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace All4Fit.Tests
+{
+    [TestClass()]
+    public class UserTests
+    {
+        [TestMethod()]
+        public void UserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UserTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
